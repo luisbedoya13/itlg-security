8a7aa263df12825d9a11257c60f9088b7994d68f
Luis Bedoya <luisbedoya13@gmail.com>
2023-09-17 19:45:18 -0500

    Agregando controlador

--- create-token/internal/controller/create.go
+++ create-token/internal/controller/create.go
@@ -1,12 +1,22 @@
 package controller
 
 import (
+	"aidanwoods.dev/go-paseto"
 	"context"
 	"errors"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+	"itlg_security/create-token/internal/repository"
 	"itlg_security/create-token/pkg/model"
+	"time"
 )
 
-var ErrNotFound = errors.New("not found")
+var (
+	ErrNotFound = errors.New("not found")
+	ErrInvalidCreds = errors.New("invalid credentials")
+)
+
+var key = paseto.NewV4SymmetricKey()
 
 type repo interface {
 	GetUser(ctx context.Context, email string) (*model.DdbUser, error)
@@ -20,6 +30,26 @@ func New(repository repo) *Controller {
 	return &Controller{repository}
 }
 
-func (*Controller) CreatePaseto(ctx context.Context, email string, password string) (string, error) {
-	return "", nil
+func (c *Controller) CreateToken(ctx context.Context, email string, password string) (string, error) {
+	user, err := c.repository.GetUser(ctx, email)
+	if err != nil && errors.Is(err, repository.ErrNotFound) {
+		return "", ErrNotFound
+	} else if err != nil {
+		return "", err
+	}
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err != nil {
+		return "", ErrInvalidCreds
+	}
+	now := time.Now()
+	token := paseto.NewToken()
+	token.SetIssuer("itlg-security")
+	token.SetSubject(user.ID)
+	token.SetAudience("itlg-fe")
+	token.SetNotBefore(now)
+	token.SetIssuedAt(now)
+	token.SetExpiration(now.Add(5 * time.Minute))
+	token.SetJti(uuid.New().String())
+	encrypted := token.V4Encrypt(key, nil)
+	return encrypted, nil
 }
--- go.mod
+++ go.mod
@@ -12,6 +12,8 @@ require (
 )
 
 require (
+	aidanwoods.dev/go-paseto v1.5.0 // indirect
+	aidanwoods.dev/go-result v0.1.0 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.13.11 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.41 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.35 // indirect
@@ -24,5 +26,8 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.15.6 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.21.5 // indirect
 	github.com/aws/smithy-go v1.14.2 // indirect
+	github.com/google/uuid v1.3.1 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	golang.org/x/crypto v0.13.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
 )
